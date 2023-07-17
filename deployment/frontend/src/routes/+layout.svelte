d771717413ec33670542e3454f27326373812ed5
ejmockler <emockler@ucsc.edu>
2023-07-16 23:28:58 -0700

    stub frontend

---
+++ deployment/frontend/src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<main>
+  <slot />
+</main>
+
+<style lang="scss">
+  :global(html),
+  :global(body) {
+    background: linear-gradient(343deg, #07c090, #1971a9, #934646, #00ecf4);
+    background-size: 800% 800%;
+
+    animation: backgroundSwoop 14s ease infinite;
+  }
+
+  @keyframes backgroundSwoop {
+    0% {
+      background-position: 0% 17%;
+    }
+    50% {
+      background-position: 100% 84%;
+    }
+    100% {
+      background-position: 0% 17%;
+    }
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100%;
+  }
+</style>
--- deployment/frontend/src/routes/+page.svelte
+++ deployment/frontend/src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { enhance } from "$app/forms";
+</script>
+
+<section>
+  <h1>AgGRU-Check</h1>
+  <form
+    method="POST"
+    action="?/predict"
+    use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+      // `formElement` is this `<form>` element
+      // `formData` is its `FormData` object that's about to be submitted
+      // `action` is the URL to which the form is posted
+      // calling `cancel()` will prevent the submission
+      // `submitter` is the `HTMLElement` that caused the form to be submitted
+      return async ({ result, update }) => {
+        // `result` is an `ActionResult` object
+        // `update` is a function which triggers the default logic that would be triggered if this callback wasn't set
+      };
+    }}
+  >
+    <textarea
+      name="sequences"
+      aria-label="Enter up to 5 protein sequences (raw, FASTA or FASTQ)"
+      placeholder="Enter up to 5 protein sequences (raw, FASTA or FASTQ)"
+    />
+    <button>Submit</button>
+  </form>
+</section>
+
+<style lang="scss">
+  h1 {
+    color: white;
+    font-size: xx-large;
+    text-align: center;
+  }
+
+  section {
+    margin: 4em;
+  }
+  form {
+    display: flex;
+    justify-content: flex-start;
+
+    flex-direction: column;
+    gap: 0.75rem;
+  }
+
+  button {
+    height: 2rem;
+    background-color: slategray;
+    color: white;
+  }
+  textarea {
+    min-height: 20vh;
+    min-width: 60vw;
+    color: white;
+    background-color: darkslategray;
+  }
+</style>
